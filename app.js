207068917c865365cac72912607ba522cc2ce276
mrbestman <114587575+mrbestman@users.noreply.github.com>
2022-09-29 13:13:45 -0700

    app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+{
+    "name": "gbaemulator",
+    "description": "gbaem is a emulator for gba.",
+    "repository": "https://github.com/mrbestman/gbaem",
+    "logo": "https://raw.githubusercontent.com/mrbestman/gbaem/master/views/assets/img/icon.png",
+    "keywords": ["gbaem", "womginx", "corrosion", "mrbestman", "node", "proxy", "gba", "webproxy", "games"]
+}
